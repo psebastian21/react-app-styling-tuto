bdbb6e352617327d8ff6690a5260682c3c619545
Paulo García <psebastian21@gmail.com>
2023-05-24 23:33:18 -0500

    Added dynammyc css classes

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,19 +5,29 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [inputIsValid, setInputIsValid] = useState(true)
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0){
+      setInputIsValid(true)
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length === 0){
+      setInputIsValid(false)
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
+  //const inputStyle = {borderColor: inputIsValid ? 'black' : 'red', background: inputIsValid ? 'transparent' : 'salmon'}
+  //const courseLabelStyle = {color: inputIsValid ? 'black' : 'red'}
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${inputIsValid ? '' : 'invalid'}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
